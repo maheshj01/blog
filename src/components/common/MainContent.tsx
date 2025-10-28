bb14e2aef4256ed6dca099a6758132200cf3bb90
mahesh jamdade <maheshmn121@gmail.com>
2025-10-27 22:19:47 -0400

    Update main content

---
+++ src/components/common/MainContent.tsx
@@ -0,0 +1,33 @@
+import styles from "../../pages/index.module.css";
+import Constants from "../../pages/constants";
+import Button from "./Button";
+
+interface HomepageHeaderProps {
+    title: string;
+    description: string;
+}
+
+export default function MainContent({ title, description }:
+
+    HomepageHeaderProps) {
+    return (
+        <header className={`${styles.heroBanner}`}>
+            <p className="text-2xl font-medium">{title}</p>
+            <p className="text-2xl font-medium">{description}</p>
+            <div className={`${styles.buttons}`}>
+                <Button
+                    onClick={() => {
+                        window.open(Constants.PORTFOLIO_URL, "_blank")
+                    }}>{Constants.AboutMe}</Button>
+                <div style={{ width: 20 }}></div>
+                <Button
+                    onClick={() => {
+                        window.open(Constants.TWITTER_URL, "_blank")
+                    }}
+                    className={styles.button}>Twitter</Button>
+            </div>
+        </header >
+    );
+}
+
+
---
+++ src/pages/constants.ts
@@ -0,0 +1,27 @@
+//
+
+class Constants {
+    static readonly BASE_URL = "https://blog.maheshjamdade.com";
+    static readonly PORTFOLIO_URL = "https://maheshjamdade.com";
+    static readonly TWITTER_URL = "https://x.com/maheshj01";
+    static readonly THREADS_URL = "https://threads.net/@maheshj01";
+    static readonly GITHUB_URL = "https://github.com/maheshj01";
+    static readonly LINKEDIN_URL = "https://www.linkedin.com/in/maheshjamdade/";
+    static readonly YOUTUBE_URL = "https://www.youtube.com/@maheshj01";
+    static readonly INSTAGRAM_URL = "https://www.instagram.com/maheshjamdade/";
+    static readonly FACEBOOK_URL = "https://www.facebook.com/maheshj01/";
+
+
+    static readonly Title = "Mahesh Jamdade";
+    static readonly About = "Hello there! I'm Mahesh. Welcome to my personal blog. Here, I share my insights and reflections on life, career and code. Everything I have ever written on the web at one place.\n\nFeel free to explore and enjoy!";
+    static readonly AboutDescription = "Feel free to explore and enjoy!";
+    static readonly AboutMe = "About Me";
+    static readonly Blog = "Blog";
+    static readonly Contact = "Contact";
+    static readonly Projects = "Projects";
+    static readonly Resume = "Resume";
+    static readonly Services = "Services";
+    static readonly Skills = "Skills";
+}
+
+export default Constants;
